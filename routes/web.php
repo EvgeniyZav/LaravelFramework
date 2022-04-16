5e6bfbacc4e77fa1fcfb7526c2a50a36f606e1ba
is41-zavolyntseves <zavevgeniy@yandex.ru>
2022-04-16 13:57:05 +0500

    Version 1.0

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-name', function () {
+    return 'Заволынцев Евгений Сергеевич';
+});
+
+Route::get('/my-friend', function () {
+    return 'Маеров Салават';
+});
+Route::get('/get-friend/{Name}', function ($Name) {
+    return 'Мой друг: '.$Name;
+});
+
+Route::get('/my-city/{city}', function ($city) {
+    return 'Мой город: '.$city;
+});
+
+
+
+Route::get('/Level/{lvl}', function ($lvl) {
+return 'Нубас'.$lvl;
+})->where('lvl', '[0-25]');
+
+Route::get('/Level/{lvl}', function ($lvl) {
+    return 'Специалист'.$lvl;
+})->where('lvl', '[26-50]');
+
+Route::get('/Level/{lvl}', function ($lvl) {
+    return 'Босс'.$lvl;
+})->where('lvl', '[51-75]');
+
+Route::get('/Level/{lvl}', function ($lvl) {
+    return 'Король'.$lvl;
+})->where('lvl', '[76-98]');
+
+Route::get('/Level/{lvl}', function ($lvl) {
+    return 'Нубас'.$lvl;
+})->where('lvl', '[99]');
